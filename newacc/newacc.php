0e58e2ad42276582e87667e94d6e5ec90cd80454
Dmitriy Kovaliov <dmitriy.kovaliov@intel.com>
2016-02-25 16:08:38 +0200

    added new user template

--- newacc/newacc.php
+++ newacc/newacc.php
@@ -6,7 +6,10 @@ session_start();
 <html>
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="newacc.css">
   <link rel="stylesheet" type="text/css" href="../style.css">
+  
+  <script src="newaccjs"></script>
   <title>בית ספר בינלאומי</title>
 </head>
 <body>
@@ -41,21 +44,47 @@ session_start();
     </ul>
   </nav>
   <?php
-  if (isset($_SESSION['curruser']))
+  if (!isset($_SESSION['curruser']))
   {
     ?>
     <div class="main">
-      <h1>Here you can add new user</h1>
+      <div class="one">
+        <div class="register">
+          <h3>צור משתמש חדש</h3>
+          <form id="reg-form">
+            <div>
+              <input type="text" id="username" spellcheck="false" autofocus="" style="text-align:right;" placeholder="שם משתמש"/>
+              <label for="name">שם משתמש</label>
+            </div>
+            <div>
+              <input type="text" id="email" spellcheck="false" style="text-align:right;" placeholder="אי-מייל"/>
+              <label for="email">דוא"ל</label>
+            </div>
+            <div>
+              <input type="password" style="text-align:right;" id="סיסמא" />
+              <label for="password">סיסמא</label>
+            </div>
+            <div>
+              <input type="password" style="text-align:right;" id="הכנס סיסמא שנית" />
+              <label for="password-again">סיסמא שנית</label>
+            </div>
+            <div>
+              <input type="submit" value="צור משתמש" id="create-account" class="button"/>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    <?php 
-  } else {
-    ?>
-    <div class="loggedout">נא הירשם למערכת</div>
-    <?php
-  }
+  </div>
+  <?php 
+} else {
   ?>
-  <footer>
-    כל הזכויות שמורות 
-  </footer>
+  <div class="loggedout">נא הירשם למערכת</div>
+  <?php
+}
+?>
+<footer>
+  כל הזכויות שמורות 
+</footer>
 </body>
 </html>
